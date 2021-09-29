b33c8fd3ecb8b0e4150bb759b7e7ecae14fb887d
Dobriyan Petkov <dobriyanp@yahoo.com>
2021-09-29 23:15:33 +0100

    modified excercise code

--- ExerciseDOM/07. Hell's Kitchen/app.js
+++ ExerciseDOM/07. Hell's Kitchen/app.js
@@ -3,56 +3,49 @@ function solve() {
 
    function onClick () {
       let restaurants = JSON.parse(document.querySelector('#inputs textarea').value);
-      let restaurantsCatalog = restaurants
-      .reduce((catalog, r, i, arr) => reduceCatalog(catalog, r, i, arr), {});
-      let sortedCatalog = Object.keys(restaurantsCatalog)
-      .sort((key1, key2) => restaurantsCatalog[key2].avgPrice() - restaurantsCatalog[key1].avgPrice())
-      
-      let bestRestaurant = restaurantsCatalog[sortedCatalog[0]];
-      let salaryCatalog = Object.keys(bestRestaurant.employees);
-      let sortedSalary = salaryCatalog.sort((key1, key2) => bestRestaurant.employees[key2] - bestRestaurant.employees[key1]);
+      let catalog = restaurants
+         .reduce((catalog, r) => reduceCatalog(catalog, r), {});
 
-      let firstRow = `Name: ${sortedCatalog[0]} Average Salary: ${bestRestaurant.avgPrice()} Best Salary: ${bestRestaurant.employees[sortedSalary[0]].toFixed(2)}`
-      let secondRow = ''
+      let sortedCatalog = Object.keys(catalog)
+         .sort((key1, key2) => catalog[key2].avgPrice() - catalog[key1].avgPrice());
+      let bestRestaurant = catalog[sortedCatalog[0]];
+      let sortedSalary = Object.keys(bestRestaurant.employees)
+         .sort((key1, key2) => bestRestaurant.employees[key2] - bestRestaurant.employees[key1]);
+      
+      let restaurantInfo = `Name: ${sortedCatalog[0]} Average Salary: ${bestRestaurant.avgPrice()} Best Salary: ${bestRestaurant.employees[sortedSalary[0]].toFixed(2)}`
+      let workersInfo = ''
       sortedSalary.forEach((key) => {
-         secondRow += `Name: ${key} With Salary: ${bestRestaurant.employees[key]} `;
+         workersInfo += `Name: ${key} With Salary: ${bestRestaurant.employees[key]} `;
       });
 
-      document.querySelectorAll('#outputs div p')[0].textContent = firstRow;
-      document.querySelectorAll('#outputs div p')[1].textContent = secondRow;
+      document.querySelectorAll('#outputs div p')[0].textContent = restaurantInfo;
+      document.querySelectorAll('#outputs div p')[1].textContent = workersInfo;
 
       function avgPrice() {
-         let salaries = Object.values(this.employees)
-         let avgPrice = salaries.reduce((avg, s) => avg+= s, 0);
-         avgPrice /= salaries.length;
+         let salaries = Object.values(this.employees);
+         let totalSalaries = salaries.reduce((avg, s) => avg+= s);
+         let avgPrice = totalSalaries / salaries.length;
+
          return avgPrice.toFixed(2);
       }
-
       
-      function reduceCatalog(catalog, r, i, arr) {
-         let [restaurantName, employees] = [...r.split(' - ')];
+      function reduceCatalog(catalog, r) {
+         let [restaurantName, employees] = r.split(' - ');
          employees = employees.split(', ');
+
          let exists = catalog[restaurantName];
- 
          if (!exists) {
-             catalog[restaurantName] = {"employees": {}, avgPrice};
-               employees.forEach((e) => {
-                   let [eName, eSalary] = e.split(' ');
-                   catalog[restaurantName]["employees"][eName] = Number(eSalary);
-               })              
-         } else {
-               employees.forEach((e) => {
-                   let [eName, eSalary] = e.split(' ');
-                   catalog[restaurantName]["employees"][eName] = Number(eSalary);
-               });
+               catalog[restaurantName] = { "employees": {}, avgPrice };
          }
- 
+
+         employees.forEach((e) => {  
+            let [eName, eSalary] = e.split(' ');
+            catalog[restaurantName]["employees"][eName] = Number(eSalary);
+         });
+
          return catalog;
              
        }
-      
-
-      
    }
 }
 
