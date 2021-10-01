8ac45a78a7dc3c293282cc4a9769223f3b35d2f8
Dobriyan Petkov <dobriyanp@yahoo.com>
2021-10-01 15:05:05 +0100

    completed DOM-manipulations

--- ExerciseDOM/08. Generate Report/report.js
+++ ExerciseDOM/08. Generate Report/report.js
@@ -1,3 +1,22 @@
 function generateReport() {
-    //TODO
+    const selectionElements = document.querySelectorAll('input');
+    const allRows = document.querySelectorAll('tbody tr');
+
+    const result = [];
+
+    if (selectionElements.length < 1) {
+        return;
+    }
+
+    for (let i = 0; i < allRows.length; i++) {
+        let newObj = {}
+        for (let y = 0; y < selectionElements.length; y++ ) {
+            if (selectionElements[y].checked) {
+                newObj[selectionElements[y].name] = allRows[i].children[y].textContent;
+            }
+        }
+        result.push(newObj)
+    }
+
+    document.getElementById('output').textContent = JSON.stringify(result);
 }
