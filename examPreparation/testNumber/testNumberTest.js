0be3176067c057ae3f32402387ee6b295cac8666
Dobriyan Petkov <dobriyanp@yahoo.com>
2021-10-23 09:39:31 +0100

    completed exam preparation

--- examPreparation/testNumber/testNumberTest.js
+++ examPreparation/testNumber/testNumberTest.js
@@ -1,3 +1,25 @@
 const { expect } = require('chai');
 const { testNumbers } = require('./testNumbers');
 
+describe('test function testNumbers', () => {
+    describe('test sumNumber', () => {
+        it('has valid input whole number' ,() => {
+            
+        }) 
+
+        
+
+        it('has valid input decimail point number')
+    })
+
+    describe('test numberChecker', () => {
+        
+    })
+
+    describe('test averageSumArray', () => {
+        
+    })
+
+
+
+})
